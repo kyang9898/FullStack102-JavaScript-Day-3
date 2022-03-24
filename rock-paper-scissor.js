ac53d828bc2f2f4fd341376c78469ff7289a245c
Keng Yang <kyang0908@gmail.com>
2022-03-23 21:16:27 -0400

    this is an update

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -1,52 +1,83 @@
 const hands = ['rock' , 'paper', 'scissor'];
+
 function getHand() {
     var randomNumber = parseInt(Math.random()*10)%3; //gets random number 
+    // console.log(hands[randomNumber]);
     return hands[randomNumber]; //returns hands with random number which is r, p, or s 
 }
-// let handOne = [{name: "Keng" , hands: getHand()}];
-// let handTwo = [{name: "Yang" , hands: getHand()}];
+
+let handOne = {name: "Cat" , getHand: function() { return getHand(); }}; // 
+let handTwo = {name: "Dog" , getHand: function() { return getHand(); }}; // 
+let handOneWin = 0;
+let handTwoWin = 0;
+let totalGameCount = 0;
 
 function playRound(handOne, handTwo){ //play a round 
-    // handOne.hands = handOne.hands;
-    // handTwo.hands = handTwo.hands;
-    // console.log("1" + handOne.hands);
-    // console.log("2" + handTwo.hands);
-    console.log(handOne.name + " played " + handOne.hands); //logs name of player1 and what they played
-    console.log(handTwo.name + " played " + handTwo.hands); //logs name of player and what they played
-    if(handOne.hands == handTwo.hands){ //If hands one and hands two are equal, it comes out as a tie. 
-        console.log("It is a tie");
+    var handOneChoice = handOne.getHand();
+    var handTwoChoice = handTwo.getHand();
+
+    console.log(handOne.name + " played " + handOneChoice); //logs name of player1 and what they played
+    console.log(handTwo.name + " played " + handTwoChoice); //logs name of player and what they played
+
+    if(handOneChoice == handTwoChoice){ //If hands one and hands two are equal, it comes out as a tie. 
+        totalGameCount +=1;
+        console.log(totalGameCount + ". "  + "It is a tie");
     }
-    else if (handOne.hands == "rock"){
-        if (handTwo.hands == "paper") {
-            console.log(handTwo.name + " wins");
+    else if (handOneChoice == "rock"){
+        if (handTwoChoice == "paper") {
+            handTwoWin +=1; 
+            totalGameCount +=1;
+            console.log(totalGameCount + ". " + handTwo.name + " wins game ");
         }  
-        if (handTwo.hands == "scissor") {
-            console.log(handOne.name + " wins");
+        if (handTwoChoice == "scissor") {
+            handOneWin +=1;
+            totalGameCount +=1;
+            console.log(totalGameCount + ". " + handOne.name + " wins game");
         } 
 
-    } else if (handOne.hands == "scissor"){
-        if (handTwo.hands == "paper") {
-            console.log(handOne.name + " wins");
+    } else if (handOneChoice == "scissor"){
+        if (handTwoChoice == "paper") {
+            handOneWin +=1;
+            totalGameCount +=1;
+            console.log(totalGameCount + ". " + handOne.name + " wins game ");
         }  
-        if (handTwo.hands == "rock") {
-            console.log(handTwo.name + " wins");
+        if (handTwoChoice == "rock") {
+            handTwoWin +=1;
+            totalGameCount +=1;
+            console.log(totalGameCount + ". "  + handTwo.name + " wins game ");
         } 
 
-    } else if (handOne.hands == "paper"){
-        if (handTwo.hands == "scissor") {
-            console.log(handTwo.name + " wins");
+    } else if (handOneChoice == "paper"){
+        if (handTwoChoice == "scissor") {
+            handTwoWin +=1;
+            totalGameCount +=1;
+            console.log(totalGameCount + ". "  + handTwo.name + " wins game ");
         }  
-        if (handTwo.hands == "rock") {
-            console.log(handOne.name + " wins");
+        if (handTwoChoice == "rock") {
+            handOneWin +=1;
+            totalGameCount +=1;
+            console.log(totalGameCount + ". " + handOne.name + " wins game ");
         } 
     } else {
         console.log("ERROR");
     }
 }
-function playgame(player1 , player2 , playUntil){
-    
+function playGame(handOne , handTwo , playUntil){
+    if(handOneWin < playUntil && handTwoWin < playUntil) {
+        playRound(handOne, handTwo);
+        playGame(handOne , handTwo , playUntil);
+    }
 }
-let handOne = {name: "Cat" , hands: getHand()};
-let handTwo = {name: "Dog" , hands: getHand()};
+function result(){
+    playGame(handOne , handTwo , 5);
+    if (handOneWin > handTwoWin) {
+        console.log(handOne.name + "wins game of " + handOneWin + " out of " + totalGameCount);
+    } else if (handOneWin < handTwoWin) {
+        console.log(handTwo.name + "wins game of " + handTwoWin + " out of " + totalGameCount);
+    } else 
+    {
+        console.log("tie game");
+    }
+}
 
-playRound(handOne, handTwo);
+result();
